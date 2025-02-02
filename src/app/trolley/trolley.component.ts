72f4080ef4decfdd12f9ae81b62fd6c75b7cb3e8
unknown <sietkod@tcd.ie>
2025-02-02 20:02:10 +0000

    added angular app to frontend branch with homepage

---
+++ src/app/trolley/trolley.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-trolley',
+  imports: [],
+  templateUrl: './trolley.component.html',
+  styleUrl: './trolley.component.css'
+})
+export class TrolleyComponent {
+
+}
