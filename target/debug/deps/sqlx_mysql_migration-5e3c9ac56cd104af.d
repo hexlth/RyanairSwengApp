6967d809c0f5b83742dce1911eb5a66cc269d644
Your Name <you@example.com>
2025-02-21 06:19:09 +0000

    go program to take csv and put into sqlite db

--- target/debug/deps/sqlx_mysql_migration-5e3c9ac56cd104af.d
+++
@@ -1,5 +0,0 @@
-/Users/hexlth/sqlx-mysql-migration/target/debug/deps/libsqlx_mysql_migration-5e3c9ac56cd104af.rmeta: src/main.rs
-
-/Users/hexlth/sqlx-mysql-migration/target/debug/deps/sqlx_mysql_migration-5e3c9ac56cd104af.d: src/main.rs
-
-src/main.rs:
