43b22dd7e66b3c2cd91389f246e31539c18f2a91
Joseph McSweeney <jomcswee@tcd.ie>
2025-02-21 06:03:31 +0000

    Revert "go program to take in csv values and insert into sqlite db"
    This reverts commit 2c46f3c791005596aff340bb7c4c1922ede85374

--- go.mod
+++
@@ -1,5 +0,0 @@
-module csv-to-db
-
-go 1.24.0
-
-require github.com/mattn/go-sqlite3 v1.14.24
---
+++ migrations/20250123164857_init.up.sql
@@ -0,0 +1,5 @@
+CREATE TABLE tbl1 (
+    'id' int NOT NULL,
+    'itemName' varchar(100) NOT NULL,
+    PRIMARY KEY ('id')
+);
