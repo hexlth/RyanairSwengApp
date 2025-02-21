b378f44fae38a4e4ba41f698e4192a0f55ea2ff0
Joseph McSweeney <jomcswee@tcd.ie>
2025-02-21 06:36:44 +0000

    Update file csvtodb.go

--- csvtodb.go
+++ csvtodb.go
@@ -11,6 +11,11 @@ import (
 
 func main() {
 
+	// the code assumes :
+	// 1. that there is already a database created and that they contain the columns that are referred to in lines 27 
+	// 2. the csv has exactly 7 values
+
+
 	// open database and csv, and read csv
 	database, _ := sql.Open("sqlite3", "database.db")
 	defer database.Close()
@@ -20,11 +25,13 @@ func main() {
 	csvValues, _ := csvReader.ReadAll()
 
 	// injection sql
+	// each quote marked word represents a column of the db that the csv values enter via
+
 	insertSQL := `INSERT INTO data ("Index", "Type", "EPC", "ID", "UserData", "ReservedData", "TotalCount", "ReadTime", "InsertTime") VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)`
 	valuesSQL, _ := database.Prepare(insertSQL)
 	defer valuesSQL.Close()
 
-	// Loop through CSV records and insert data
+	// iterate through csv and inject values into database
 	for _, value := range csvValues {
 		timeNow := time.Now().Format("15:04:05 02 January 2006")
 		valuesSQL.Exec(value[0], value[1], value[2], value[3], value[4], value[5], value[6], value[7], timeNow)
