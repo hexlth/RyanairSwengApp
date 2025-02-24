c2f5b1bd989b85a7258832991beba8a7e68f4ba2
unknown <sietkod@tcd.ie>
2025-02-24 12:29:19 +0000

    added multiple view options on trolley page, added logic to change which view to be displayed on screen. Added flightnum searchbar on trolley page

--- src/app/components/trolley/trolley.component.ts
+++ src/app/components/trolley/trolley.component.ts
@@ -1,15 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ListComponent } from "../list/list.component";
-import { Chart, registerables } from 'chart.js';
 import { Item } from '../../models/item.model';
 import { ChartDataService } from '../../services/chart-data.service';
-import { SliderComponent } from '../slider/slider.component';
+import { ChartComponent } from '../chart/chart.component';
 
-Chart.register(...registerables);
 @Component({
   selector: 'app-trolley',
-  imports: [ListComponent, SliderComponent],
+  imports: [ListComponent, ChartComponent],
+  standalone: true,
   templateUrl: './trolley.component.html',
   styleUrl: './trolley.component.css'
 })
@@ -83,38 +82,21 @@ export class TrolleyComponent {
     }
   ];
 
+  // setting the route to the current activated route
+  constructor(private route: ActivatedRoute) {}
+
   // initializing flightNum in trolley page and array of Items for stock
   flightNum : string | null = "";
   stock : Item[] = [];
 
-  // arrays for the values and labels and String for graph type
-  values: number[] = [];
-  labels: String[] = [];
-  chartType: String = "bar";
-
-  // declaring chart object and its config to be used for rendering
-  config: any = {};
-  chart: any = {};
-
-  // min and max values for sliders
-  minSliderValue: number = 0;
-  maxSliderValue: number = 0;
-
-  // maximum value to cap the sliders (highest quantity from stock)
-  maxValue: number = 0;
-
-  // setting the route to the current activated route and injecting the chartDataService
-  constructor(private route: ActivatedRoute, private chartDataService: ChartDataService) {}
+  // array of screen options to choose from and declaration of current option string
+  options: string[] = ["Overview", "Products", "Graphs", "Warnings"];
+  currentOption: string = "";
 
   //initializing values/objects needed for rendering when component initialized
   ngOnInit() {
     this.flightNum = this.route.snapshot.queryParamMap.get("flightNum");
     this.stock = this.searchForData();
-    [this.values, this.labels] = this.chartDataService.initializeChartData(this.stock);
-    this.maxValue = this.chartDataService.getLargestValue();
-    this.maxSliderValue = this.maxValue;
-    this.setConfig();
-    this.chart = new Chart("chart", this.config);
   }
 
   //search function to get data associated with flightNum
@@ -123,49 +105,8 @@ export class TrolleyComponent {
     return this.flightStock.find(flight => flight.flightNum === this.flightNum)?.stock ?? [];
   }
 
-  // update chart with new values
-  updateChart() {
-    [this.values, this.labels, this.stock] = this.chartDataService.getDataRange(this.minSliderValue, this.maxSliderValue);
-    this.chart.destroy();
-    this.setConfig();
-    this.chart = new Chart("chart", this.config);
-  }
-
-  // set the chart type
-  setChartType(event: Event) {
-    this.chartType = (event.target as HTMLInputElement).value;
-  }
-
-  // setting the configurations for the chart
-  setConfig() {
-    this.config = {
-      // setting chart type
-      type: this.chartType,
-
-      data: {
-        // setting the labels
-        labels: this.labels,
-        // setting datasets, only need 1
-        datasets: [
-          {
-            label: "Quantity",
-            data: this.values,
-            backgroundColour: "blue"
-          }
-        ]
-      },
-      options: {
-        responsive: true,
-        maintainAspectRatio: false,
-      }
-    };
-  }
-
-  // functions to update the min value and max values respectively
-  updateMinValue(value: number) {
-      this.minSliderValue = value;
-  }
-  updateMaxValue(value: number) {;
-    this.maxSliderValue = value;
+  // changes current option to be displayed
+  onOptionClick(option: string) {
+    this.currentOption = option;
   }
 }
